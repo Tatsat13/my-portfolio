aaa1369f7a2450fc01211bd58418d621ac50fddf
Tatsat13 <tatsatsingh80@gmail.com>
2025-12-16 16:59:04 +0530

    Updated portfolio UI with interactive sections

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function showMessage() {
-  alert("Welcome to my website!");
+function changeText() {
+  document.querySelector(".hero p").innerText =
+    "Thanks for visiting my portfolio!";
+}
+
+function showGreeting() {
+  const name = document.getElementById("name").value;
+  const greeting = document.getElementById("greeting");
+
+  if (name === "") {
+    greeting.innerText = "Please enter your name.";
+  } else {
+    greeting.innerText = "Hello " + name + "! I will contact you soon.";
+  }
 }
